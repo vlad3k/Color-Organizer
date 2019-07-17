80c48a3caf2e7b71bd9e74219f65cf6b4a502978
vlad3k <vlad3k@bk.ru>
2019-07-17 15:31:06 +0500

    Component state management

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
+import './App.css'
+import * as serviceWorker from './serviceWorker'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import StarRating from './components/StarRating'
+
+const { render } = ReactDOM
+
+
+render(
+    <StarRating />,
+    document.getElementById('root')
+)
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
